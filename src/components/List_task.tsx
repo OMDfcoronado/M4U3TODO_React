3bac0869a0858688b144c071bbb5dfe70e1c38e8
Fredy Coronado <ing.ambfredycoronado@gmail.com>
2022-09-02 18:42:06 -0500

    finisch css

--- src/components/List_task.tsx
+++ src/components/List_task.tsx
@@ -1,4 +1,15 @@
+import { Task } from "./Task"
+
 export const List_task = () => {
     return (
-        <></>)
+        <div className="md:w-1/2 lg:w-2/5 mx-5 mb-10 md:h-screen overflow-scroll">
+            <h2 className="font-black text-3xl text-center mb-10">
+                Lista de tareas
+            </h2>
+            <Task/>
+            <Task/>
+            <Task/>
+            <Task/>
+        </div>
+    )
 }
---
+++ src/components/Task.tsx
@@ -0,0 +1,17 @@
+export const Task = () => {
+    return (
+        <div className="bg-white shadow-md px-5 py-10 rounded-lg mt-5">
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Titulo:  <span className="font-normal normal-case"> Crear API</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Fecha:  <span className="font-normal normal-case">1994-01-28</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">
+                Descripción :  <span className="font-normal normal-case">una tarea</span>
+            </p>
+        </div>
+    )
+}
